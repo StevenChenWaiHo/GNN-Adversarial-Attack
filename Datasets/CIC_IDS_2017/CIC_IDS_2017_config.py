9ae1b7496e1be6b38cf3261dc53523cb3d7a4de5
Steven Chen <shc20@ic.ac.uk>
2025-05-13 09:40:36 +0100

    12 May Progress

---
+++ Datasets/CIC_IDS_2017/CIC_IDS_2017_config.py
@@ -0,0 +1,64 @@
+class CIC_IDS_2017_Config:
+
+    DATASET_NAME = "CIC_IDS_2017"
+
+    SOURCE_IP_COL_NAME = "Source IP"
+    DESTINATION_IP_COL_NAME = "Destination IP"
+    SOURCE_PORT_COL_NAME = "Source Port"
+    DESTINATION_PORT_COL_NAME = "Destination Port"
+
+    ATTACK_CLASS_COL_NAME = "Label"
+
+    BENIGN_CLASS_NAME = "BENIGN"
+
+    TIME_COL_NAMES = ["Timestamp"]
+
+    INDEX_COL_NAME = "Flow ID"
+
+    COL_NAMES = [
+        "Flow ID", "Source IP", "Source Port", "Destination IP", "Destination Port", "Protocol", "Timestamp", "Flow Duration", 
+        "Total Fwd Packets", "Total Backward Packets", "Total Length of Fwd Packets", "Total Length of Bwd Packets", 
+        "Fwd Packet Length Max", "Fwd Packet Length Min", "Fwd Packet Length Mean", "Fwd Packet Length Std", 
+        "Bwd Packet Length Max", "Bwd Packet Length Min", "Bwd Packet Length Mean", "Bwd Packet Length Std", 
+        "Flow Bytes/s", "Flow Packets/s", "Flow IAT Mean", "Flow IAT Std", "Flow IAT Max", "Flow IAT Min", 
+        "Fwd IAT Total", "Fwd IAT Mean", "Fwd IAT Std", "Fwd IAT Max", "Fwd IAT Min", "Bwd IAT Total", 
+        "Bwd IAT Mean", "Bwd IAT Std", "Bwd IAT Max", "Bwd IAT Min", "Fwd PSH Flags", "Bwd PSH Flags", 
+        "Fwd URG Flags", "Bwd URG Flags", "Fwd Header Length", "Bwd Header Length", "Fwd Packets/s", 
+        "Bwd Packets/s", "Min Packet Length", "Max Packet Length", "Packet Length Mean", "Packet Length Std", 
+        "Packet Length Variance", "FIN Flag Count", "SYN Flag Count", "RST Flag Count", "PSH Flag Count", 
+        "ACK Flag Count", "URG Flag Count", "CWE Flag Count", "ECE Flag Count", "Down/Up Ratio", 
+        "Average Packet Size", "Avg Fwd Segment Size", "Avg Bwd Segment Size", "Fwd Header Length", 
+        "Fwd Avg Bytes/Bulk", "Fwd Avg Packets/Bulk", "Fwd Avg Bulk Rate", "Bwd Avg Bytes/Bulk", 
+        "Bwd Avg Packets/Bulk", "Bwd Avg Bulk Rate", "Subflow Fwd Packets", "Subflow Fwd Bytes", 
+        "Subflow Bwd Packets", "Subflow Bwd Bytes", "Init_Win_bytes_forward", "Init_Win_bytes_backward", 
+        "act_data_pkt_fwd", "min_seg_size_forward", "Active Mean", "Active Std", "Active Max", "Active Min", 
+        "Idle Mean", "Idle Std", "Idle Max", "Idle Min", "Label"
+    ]
+
+    CHOSEN_COLS = [
+        'Source Port', 'Destination Port', 'Bwd Packet Length Min', 'Subflow Fwd Packets',
+        'Total Length of Fwd Packets', 'Fwd Packet Length Mean', 'Total Length of Fwd Packets',
+        'Fwd Packet Length Std', 'Fwd IAT Min', 'Flow IAT Min', 'Flow IAT Mean', 'Bwd Packet Length Std',
+        'Subflow Fwd Bytes', 'Flow Duration', 'Flow IAT Std', 'Active Min','Active Mean', 'Bwd IAT Mean',
+        'Subflow Bwd Bytes', 'Init_Win_bytes_forward', 'ACK Flag Count','Fwd PSH Flags','SYN Flag Count',
+        'Flow Packets/s', 'PSH Flag Count', 'Average Packet Size', 'Protocol'
+    ]
+
+    DROP_COLS = set(COL_NAMES) - set(CHOSEN_COLS) - set(TIME_COL_NAMES)
+    DROP_COLS = []
+
+    COLS_TO_NORM = [
+        # 'Source Port', 'Destination Port', 
+        'Bwd Packet Length Min', 'Subflow Fwd Packets',
+        'Total Length of Fwd Packets', 'Fwd Packet Length Mean', 'Total Length of Fwd Packets',
+        'Fwd Packet Length Std', 'Fwd IAT Min', 'Flow IAT Min', 'Flow IAT Mean', 'Bwd Packet Length Std',
+        'Subflow Fwd Bytes', 'Flow Duration', 'Flow IAT Std', 'Active Min','Active Mean', 'Bwd IAT Mean',
+        'Subflow Bwd Bytes', 'Init_Win_bytes_forward', 'ACK Flag Count','Fwd PSH Flags','SYN Flag Count',
+        'Flow Packets/s', 'PSH Flag Count', 'Average Packet Size'
+    ]
+
+    CATEGORICAL_COLS = [
+        "Protocol", 
+    ]
+
+    assert (set(COLS_TO_NORM) | set(CATEGORICAL_COLS)).issubset(set(COL_NAMES) - set(DROP_COLS)), f"Some columns are not in the right place. Please check the column names in {DATASET_NAME}_config.py ."
---
+++ Datasets/CIC_IDS_2017/combine_and_split.py
@@ -0,0 +1,149 @@
+import os
+import joblib
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from CIC_IDS_2017_config import CIC_IDS_2017_Config
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+# Paths
+input_dir = './TrafficLabelling/'
+output_all_raw_path = './All/all_raw.csv' 
+output_all_downsampled_path = './All/all_downsampled.csv' 
+output_train_path = './Train/train_scaled.csv'
+output_eval_path = './Eval/eval_scaled.csv'
+
+SOURCE_IP_COL_NAME = CIC_IDS_2017_Config.SOURCE_IP_COL_NAME
+DESTINATION_IP_COL_NAME = CIC_IDS_2017_Config.DESTINATION_IP_COL_NAME
+SOURCE_PORT_COL_NAME = CIC_IDS_2017_Config.SOURCE_PORT_COL_NAME
+DESTINATION_PORT_COL_NAME = CIC_IDS_2017_Config.DESTINATION_PORT_COL_NAME
+
+ATTACK_CLASS_COL_NAME = CIC_IDS_2017_Config.ATTACK_CLASS_COL_NAME
+INDEX_COL_NAME = CIC_IDS_2017_Config.INDEX_COL_NAME
+
+# Combine all CSV files in the input directory
+all_files = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if f.endswith('.csv')]
+
+for file in all_files:
+    try:
+        df = pd.read_csv(file, header=0, encoding='cp1252')
+        # df_list.append(df)
+    except UnicodeDecodeError as e:
+        print(f"UnicodeDecodeError in file: {file}")
+        print(f"Error: {e}")
+
+df_list = [pd.read_csv(file, header=0, encoding='cp1252') for file in all_files]
+df_full = pd.concat(df_list)
+
+df_full.columns = df_full.columns.str.strip()
+
+# Strip whitespaces from all string columns
+df_full = df_full.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+
+df_full.fillna(0, inplace=True)
+
+# # Replace – with -
+df_full[ATTACK_CLASS_COL_NAME] = (
+    df_full[ATTACK_CLASS_COL_NAME]
+    .str.replace('ï¿½', '-', regex=False)
+    .str.replace('–', '-', regex=False)  # en dash
+    .str.replace('—', '-', regex=False)  # em dash
+    .str.strip()
+)
+
+print(df_full[ATTACK_CLASS_COL_NAME].value_counts())
+
+if 'Web Attack - Brute Force' not in df_full[ATTACK_CLASS_COL_NAME].unique():
+    raise ValueError("Web Attack - Brute Force not found in the dataset")
+
+df_full.to_csv(output_all_raw_path, index=False, header=True)
+
+# Drop unnecessary columns
+df_full.drop(columns=CIC_IDS_2017_Config.DROP_COLS,inplace=True)
+
+df_full[SOURCE_IP_COL_NAME] = df_full[SOURCE_IP_COL_NAME].apply(str)
+df_full[SOURCE_PORT_COL_NAME] = df_full[SOURCE_PORT_COL_NAME].apply(str)
+df_full[DESTINATION_IP_COL_NAME] = df_full[DESTINATION_IP_COL_NAME].apply(str)
+df_full[DESTINATION_PORT_COL_NAME] = df_full[DESTINATION_PORT_COL_NAME].apply(str)
+
+print(df_full.head)
+
+df_full = df_full.reset_index()
+df_full.replace([np.inf, -np.inf], np.nan,inplace = True)
+df_full.fillna(0,inplace = True)
+df_full.drop(columns=[INDEX_COL_NAME],inplace=True)
+
+# Categorical columns to one-hot encode
+df_full = pd.get_dummies(df_full, columns=CIC_IDS_2017_Config.CATEGORICAL_COLS)
+df_full = df_full.applymap(lambda x: int(x) if isinstance(x, (bool, np.bool_)) else x)
+
+
+print(f"=====Columns=====")
+print(df_full.columns.tolist())
+print('No. of Columns: ', len(df_full.columns.tolist()))
+
+def check_numeric_issues(df, cols_to_norm):
+    for col in cols_to_norm:
+        try:
+            # Try to coerce to numeric
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+            
+            # Try to clip the column
+            df[col] = df[col].clip(lower=-1e9, upper=1e9)
+            
+        except Exception as e:
+            print(f"❌ Column '{col}' failed with error: {e}")
+            print(f"  - Sample values: {df[col].dropna().unique()[:5]}")
+            print(f"  - Data type: {df[col].dtype}")
+            continue
+
+    print("\n✅ All other columns processed successfully.")
+
+check_numeric_issues(df_full, CIC_IDS_2017_Config.COLS_TO_NORM)
+
+# Downsample the data by 10%
+normal_traffic_df = df_full[df_full[CIC_IDS_2017_Config.ATTACK_CLASS_COL_NAME] == CIC_IDS_2017_Config.BENIGN_CLASS_NAME]
+downsampled_normal_df = normal_traffic_df.sample(frac=0.1, random_state=42)
+downsampled_df = pd.concat([downsampled_normal_df, df_full[df_full[CIC_IDS_2017_Config.ATTACK_CLASS_COL_NAME] != CIC_IDS_2017_Config.BENIGN_CLASS_NAME]])
+
+
+print("downsampled: ", df_full.head(5))
+# Save the downsampled and sorted data to a new CSV file
+downsampled_df.to_csv(output_all_downsampled_path, index=False)
+
+# Split the dataset
+train_df, test_df = train_test_split(
+    downsampled_df,
+    test_size=0.15,
+    stratify=downsampled_df[CIC_IDS_2017_Config.ATTACK_CLASS_COL_NAME],
+    random_state=42
+)
+
+scaler = StandardScaler()
+cols_to_norm = CIC_IDS_2017_Config.COLS_TO_NORM
+print(train_df[cols_to_norm].describe()) # Check if there's any too large value
+
+train_df[cols_to_norm] = scaler.fit_transform(train_df[cols_to_norm])
+test_df[cols_to_norm] = scaler.transform(test_df[cols_to_norm])
+
+# Save the scaler to a file
+joblib.dump(scaler, 'scaler.pkl')
+
+# Save the split datasets to CSV
+os.makedirs('./Train', exist_ok=True)
+os.makedirs('./Eval', exist_ok=True)
+
+train_df.to_csv(output_train_path, index=False, header=True)
+test_df.to_csv(output_eval_path, index=False, header=True)
+
+print(f"=====Train dataset=====")
+print(f"Train dataset shape: {train_df.shape}")
+print(train_df[CIC_IDS_2017_Config.ATTACK_CLASS_COL_NAME].value_counts())
+
+
+print(f"=====Eval dataset=====")
+print(f"Eval dataset shape: {test_df.shape}")
+print(test_df[CIC_IDS_2017_Config.ATTACK_CLASS_COL_NAME].value_counts())
+
+print(f"Train dataset saved to {output_train_path}")
+print(f"Eval dataset saved to {output_eval_path}")
